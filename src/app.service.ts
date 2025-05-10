8cb18bf0fe2ce7179f07ba9856fa40770ecdd435
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2025-05-10 19:43:44 +0800

    Production Origin

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '🚀 Backend aktif! Akses dokumentasi API di http://localhost:3000/';
+    return '🚀 Backend aktif! Akses dokumentasi API di https://alarm-tag.vercel.app/';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // ✅ Aktifkan CORS
   app.enableCors({
-    origin: 'http://localhost:3001', // sesuaikan dengan domain frontend kamu
+    origin: `https://alarm-tag.vercel.app/`, // sesuaikan dengan domain frontend kamu
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
